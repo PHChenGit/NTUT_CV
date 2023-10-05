bc476a3b3f5e0d3c571a7a1306f860a4c7c911ab
PHChenGit <chenpohsun1@gmail.com>
2023-10-05 14:51:59 +0800

    chore: Setup the project sture

--- 112/CV_HW1/run_docker.sh
+++ 112/CV_HW1/run_docker.sh
@@ -1,1 +1,35 @@
-docker run -it --rm -d -v $(pwd):/app/ --name=ntut_cv_hw1 tokohsun/opencv48-py310:latest
+
+xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+
+# docker run -it \
+#     --name=ntut_cv_hw1 \
+#     -d \
+#     --rm \
+#     --env="DISPLAY=unix$DISPLAY" \
+#     --env="QT_X11_NO_MITSHM=1" \
+#     --env="XAUTHORITY=$XAUTH" \
+#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#     --volume="$XAUTH:$XAUTH" \
+#     --volume="$(pwd):/app/" \
+#     --net=host \
+#     --privileged \
+#     --gpus all \
+#     tokohsun/opencv:4.8.1
+
+docker run -it \
+    --name=ntut_cv_hw1 \
+    -d \
+    --rm \
+    --env="DISPLAY=unix$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$XAUTH:$XAUTH" \
+    --volume="$(pwd):/app/" \
+    --net=host \
+    --privileged \
+    tokohsun/opencv:4.8.1 
+
+echo "Done!!"
