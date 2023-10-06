726e8fe73766e2c3e335fd5784c99e16fd2346c2
PHChenGit <chenpohsun1@gmail.com>
2023-10-06 18:36:08 +0800

    feat: Do convolution with kernel [-1, -1, -1, -1, 8, -1, -1, -1, -1]

--- 112/CV_HW1/112598072_hw1/112598072_hw1.py
+++ 112/CV_HW1/112598072_hw1/112598072_hw1.py
@@ -14,7 +14,39 @@ def padding_zeros(gray_img):
 
 
 def convolution(gray_img):
-    pass
+    kernel = np.array([-1, -1, -1, -1, 8, -1, -1, -1, -1]).reshape((3, 3))
+    padding = 0
+    strides = 1
+
+    width_gray_img, height_gray_img = gray_img.shape
+    width_kernel, height_kernel = kernel.shape
+
+    width_output = int(((width_gray_img - kernel.shape[0] + 2 * padding) / strides) + 1)
+    height_output = int(((height_gray_img - kernel.shape[1] + 2 * padding) / strides) + 1)
+
+    padded_img = padding_zeros(gray_img)
+    new_img = np.zeros((width_output, height_output))
+
+    """
+    The convolution core
+    """
+    for x in range(width_gray_img):
+        if x > (width_gray_img - width_kernel):
+            break
+
+        if x % strides == 0:
+            for y in range(height_gray_img):
+                if y > (height_gray_img - height_kernel):
+                    break
+
+                try:
+                    if y % strides == 0:
+                        new_img[x, y] = (kernel * padded_img[x:x + width_kernel, y:y + height_kernel]).sum(
+                            dtype=np.uint8)
+                except:
+                    break
+
+    return new_img
 
 
 def pooling():
@@ -34,5 +66,8 @@ if __name__ == '__main__':
     np_img = np.array(img, dtype=np.float32)
     cv.imshow("Display window", img)
     q1_ans = convert_to_gray(np_img)
+
+    q2_ans = convolution(q1_ans)
+    cv.imshow("Convolution", q2_ans)
 
     k = cv.waitKey(0)
