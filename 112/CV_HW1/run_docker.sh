7a97f58ff41eaa48d0b4621aeb8b85cc48949a0f
PHChenGit <chenpohsun1@gmail.com>
2023-10-06 10:39:54 +0800

    chore: Remove redundant codes

--- 112/CV_HW1/run_docker.sh
+++ 112/CV_HW1/run_docker.sh
@@ -3,21 +3,6 @@ xhost +local:root
 
 XAUTH=/tmp/.docker.xauth
 
-# docker run -it \
-#     --name=ntut_cv_hw1 \
-#     -d \
-#     --rm \
-#     --env="DISPLAY=unix$DISPLAY" \
-#     --env="QT_X11_NO_MITSHM=1" \
-#     --env="XAUTHORITY=$XAUTH" \
-#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#     --volume="$XAUTH:$XAUTH" \
-#     --volume="$(pwd):/app/" \
-#     --net=host \
-#     --privileged \
-#     --gpus all \
-#     tokohsun/opencv:4.8.1
-
 docker run -it \
     --name=ntut_cv_hw1 \
     -d \
