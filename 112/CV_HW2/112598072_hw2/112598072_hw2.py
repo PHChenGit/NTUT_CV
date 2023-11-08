e7b8f4f7b7863208e1fb1c3abed682065d36c967
PHChenGit <chenpohsun1@gmail.com>
2023-11-08 19:56:57 +0800

    feat: Implement median filter & draw the histogram for each result images

--- 112/CV_HW2/112598072_hw2/112598072_hw2.py
+++ 112/CV_HW2/112598072_hw2/112598072_hw2.py
@@ -1,61 +1,118 @@
-import os
+import random
 import sys
 
 import cv2 as cv
 import numpy as np
-import scipy
+import matplotlib.pyplot as plt
 
 
 def convert_to_gray(img):
     return np.dot(img, [0.2989, 0.5870, 0.1140]).astype(np.uint8)
 
 
-def conv(gray_img, k, padding=0, strides=1):
-    kernel = np.flipud(np.fliplr(k))
 
-    width_gray_img, height_gray_img = gray_img.shape
-    width_kernel, height_kernel = kernel.shape
+def mean_filter(img, kernel_size=3):
+    def conv(input_img, k, padding=0, strides=1):
+        kernel = np.flipud(np.fliplr(k))
 
-    width_output = int(((width_gray_img - kernel.shape[0] + 2 * padding) // strides) + 1)
-    height_output = int(((height_gray_img - kernel.shape[1] + 2 * padding) // strides) + 1)
+        width_gray_img, height_gray_img = input_img.shape
+        width_kernel, height_kernel = kernel.shape
 
-    if padding != 0:
-        padded_img = np.zeros((gray_img.shape[0] + padding * 2, gray_img.shape[1] + padding * 2))
-        padded_img[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = gray_img
-    else:
-        padded_img = gray_img
+        width_output = int(((width_gray_img - kernel.shape[0] + 2 * padding) // strides) + 1)
+        height_output = int(((height_gray_img - kernel.shape[1] + 2 * padding) // strides) + 1)
 
-    new_img = np.zeros((width_output, height_output), dtype=np.uint8)
+        if padding != 0:
+            padded_img = np.zeros((input_img.shape[0] + padding * 2, input_img.shape[1] + padding * 2))
+            padded_img[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = input_img
+        else:
+            padded_img = input_img
 
-    """
-    The convolution core
-    """
-    for y in range(height_gray_img):
-        if y % strides == 0:
-            for x in range(width_gray_img):
-                try:
-                    if x % strides == 0:
-                        window = (kernel * padded_img[x: x + width_kernel, y: y + height_kernel]).sum()
-                        new_img[x, y] = np.clip(window, 0, 255).astype(np.uint8)
-                except:
-                    break
+        new_img = np.zeros((width_output, height_output), dtype=np.uint8)
 
-    return new_img
+        """
+        The convolution core
+        """
+        for y in range(height_gray_img):
+            if y % strides == 0:
+                for x in range(width_gray_img):
+                    try:
+                        if x % strides == 0:
+                            window = (kernel * padded_img[x: x + width_kernel, y: y + height_kernel]).sum()
+                            new_img[x, y] = np.clip(window, 0, 255).astype(np.uint8)
+                    except:
+                        break
 
+        return new_img
+    kernel = np.ones((kernel_size, kernel_size), dtype=np.float32) / (kernel_size**2)
 
-def mean_filter(img):
-    kernel = np.ones((3, 3), dtype=np.float32) / 9
-
-    res = conv(img, kernel, padding=1)
-    return res.astype(np.uint8)
-
-
-def median_filter(img):
-    pass
+    res = conv(img, kernel, padding=kernel_size//2)
+    return np.asarray(res, dtype=np.uint8)
 
 
-def img_histogram():
-    pass
+def median_filter(img, kernel_size=3):
+    def quick_sort(numbers):
+        if len(numbers) <= 1:
+            return numbers
+
+        pivot = random.choice(numbers)
+        pivot_list = []
+        left = []
+        right = []
+
+        for val in numbers:
+            if val < pivot:
+                left.append(val)
+            elif val > pivot:
+                right.append(val)
+            else:
+                pivot_list.append(val)
+
+        return quick_sort(left) + pivot_list + quick_sort(right)
+
+    def median(numbers):
+        # sorted_numbers = sorted(numbers)
+        sorted_numbers = quick_sort(numbers)
+
+        if len(numbers) % 2 == 0:
+            front_idx = len(numbers) // 2 + 1
+            next_idx = len(numbers) // 2 - 1
+            return (sorted_numbers[front_idx] + sorted_numbers[next_idx]) // 2
+
+        return sorted_numbers[len(numbers) // 2]
+
+    def conv(input_img, padding = 0, k_size=3):
+        result = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+
+        if padding != 0:
+            padded_img = np.zeros((input_img.shape[0] + padding * 2, input_img.shape[1] + padding * 2))
+            padded_img[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = input_img
+        else:
+            padded_img = input_img
+
+        for y in range(input_img.shape[1]):
+            for x in range(input_img.shape[0]):
+                window = padded_img[x: x+ k_size, y:y+k_size]
+                result[x, y] = median(window.flatten())
+        return result
+
+    return conv(img, kernel_size//2, kernel_size)
+
+
+def img_histogram(img_path, output_path, title):
+    img = cv.imread(img_path)
+    gray_img = convert_to_gray(img)
+
+    x_axis_vals = [n for n in range(0, 256, 50)]
+    x_axis_vals.append(255)
+
+    plt.xlabel('Pixel value')
+    plt.ylabel('Frequency')
+    plt.title(title)
+    plt.hist(gray_img.flatten(), 256, (0, 255), color='deepskyblue')
+
+    plt.show()
+    plt.savefig(output_path)
+
 
 if __name__ == '__main__':
     input_dir = './test_img'
@@ -64,77 +121,76 @@ if __name__ == '__main__':
     output_img_path_list = {
         "q1": [{
             "input_img": f"{input_dir}/noise1.png",
-            "output_img": f"{output_dir}/noise1_q1.png"
+            "output_img": f"{output_dir}/noise1_q1.png",
+            "window_name": "noise1_q1"
         }, {
             "input_img": f"{input_dir}/noise2.png",
-            "output_img": f"{output_dir}/noise1_q2.png"
+            "output_img": f"{output_dir}/noise2_q1.png",
+            "window_name": "noise2_q1"
         }],
         "q2": [{
             "input_img": f"{input_dir}/noise1.png",
-            "output_img": f"{output_dir}/noise1_q2.png"
+            "output_img": f"{output_dir}/noise1_q2.png",
+            "window_name": "noise1_q2"
         }, {
             "input_img": f"{input_dir}/noise2.png",
-            "output_img": f"{output_dir}/noise2_q2.png"
+            "output_img": f"{output_dir}/noise2_q2.png",
+            "window_name": "noise2_q2"
         }],
         "q3": [{
             "input_img": f"{output_dir}/noise1_q1.png",
-            "output_img": f"{output_dir}/noise1_q1_his.png"
+            "output_img": f"{output_dir}/noise1_q1_his.png",
+            "window_name": "noise1_q1_his"
         }, {
             "input_img": f"{output_dir}/noise1_q2.png",
-            "output_img": f"{output_dir}/noise1_q1_his.png"
+            "output_img": f"{output_dir}/noise1_q2_his.png",
+            "window_name": "noise1_q2_his"
         }, {
             "input_img": f"{output_dir}/noise2_q1.png",
-            "output_img": f"{output_dir}/noise2_q1_his.png"
+            "output_img": f"{output_dir}/noise2_q1_his.png",
+            "window_name": "noise2_q1_his"
         }, {
             "input_img": f"{output_dir}/noise2_q2.png",
-            "output_img": f"{output_dir}/noise2_q2_his.png"
+            "output_img": f"{output_dir}/noise2_q2_his.png",
+            "window_name": "noise2_q2_his"
         }, {
             "input_img": f"{input_dir}/noise1.png",
-            "output_img": f"{output_dir}/noise1_his.png"
+            "output_img": f"{output_dir}/noise1_his.png",
+            "window_name": "noise1_his"
         }, {
             "input_img": f"{input_dir}/noise2.png",
-            "output_img": f"{output_dir}/noise2_his.png"
+            "output_img": f"{output_dir}/noise2_his.png",
+            "window_name": "noise2_his"
         }]
     }
 
+    # for idx in range(len(output_img_path_list["q1"])):
+    #     input_img_path = output_img_path_list["q1"][idx]["input_img"]
+    #     input_img = cv.imread(input_img_path)
+    #
+    #     if input_img is None:
+    #         sys.exit(f"The image {input_img_path} not found.")
+    #
+    #     q1_ans = mean_filter(convert_to_gray(input_img), 5)
+    #     cv.imshow(output_img_path_list["q1"][idx]["window_name"], q1_ans)
+    #     cv.imwrite(output_img_path_list["q1"][idx]["output_img"], q1_ans)
+    #
+    # cv.waitKey(0)
 
-    img = cv.imread(f"{input_dir}/noise1.png")
-    if img is None:
-        sys.exit("Could not read the image.")
+    # for idx in range(len(output_img_path_list["q2"])):
+    #     input_img_path = output_img_path_list["q2"][idx]["input_img"]
+    #     input_img = cv.imread(input_img_path)
+    #
+    #     if input_img is None:
+    #         sys.exit(f"The image {input_img_path} not found.")
+    #
+    #     q2_ans = median_filter(convert_to_gray(input_img), 5)
+    #     # cv.imshow(output_img_path_list["q2"][idx]["window_name"], q2_ans)
+    #     cv.imwrite(output_img_path_list["q2"][idx]["output_img"], q2_ans)
+    #
+    # cv.waitKey(0)
 
-    gray_img = convert_to_gray(img)
-
-    mean_filter_result = mean_filter(gray_img)
-    print(f"My mean filter:\n {mean_filter_result}\n")
-    cv.imshow("my mean filter result", mean_filter_result)
-
-    # box_blur_ker = np.ones((3, 3)) / 9
-    # cv_mean_res = scipy.signal.convolve2d(gray_img, box_blur_ker, mode="same")
-    cv_mean_res = cv.blur(gray_img, (3, 3))
-
-    print(f"OpenCV mean filter:\n {cv_mean_res}\n")
-    cv.imshow("cv mean res", cv_mean_res)
-    cv.waitKey(0)
-
-    # gray_img = np.asarray([[1, 2, 0, 1, 2],
-    #                 [2, 3, 1, 1, 2],
-    #                 [1, 4, 2, 2, 0],
-    #                 [3, 2, 3, 3, 0],
-    #                 [1, 0, 0, 2, 1]
-    #                 ], dtype=np.uint8)
-    #
-    # # gray_img = convert_to_gray(img)
-    #
-    # kernel = np.ones((3, 3))/9
-    # conv_img = conv(gray_img, kernel, 1)
-    #
-    # print(f"My conv: \n{conv_img}\n")
-    #
-    # # cv_conv_img = cv.filter2D(gray_img, -1, cv.flip(kernel, -1), borderType=cv.BORDER_CONSTANT)
-    # cv_conv_img = cv.filter2D(gray_img, -1, kernel)
-    #
-    # print(f"OpenCV conv: \n{cv_conv_img}\n")
-    #
-    # sci_conv_img = scipy.signal.convolve2d(gray_img, kernel, mode="same")
-    #
-    # print(f"Scipy conv: \n{sci_conv_img}\n")
+    for idx in range(len(output_img_path_list["q3"])):
+        img_histogram(output_img_path_list["q3"][idx]["input_img"],
+                      output_img_path_list["q3"][idx]["output_img"],
+                      output_img_path_list["q3"][idx]["window_name"])
